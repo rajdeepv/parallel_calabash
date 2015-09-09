dd9169619ec722e4a8f6a1286215b3bbb99467de
Tim Baverstock <tim.baverstock@corp.badoo.com>
2015-09-09 16:37:50 +0100

    Fix patch patching

--- misc/sim_control.rb
+++ misc/sim_control.rb
@@ -9,66 +9,62 @@ require 'run_loop/sim_control'
 # Copyright whoever owns RunLoop, except for the hashed block which is presumably Badoo's but
 # we'll let you have it because we're nice.
 
-module RunLoop
-  class SimControl
-    module EraseFix
-      # @!visibility private
-      # Uses the `simctl erase` command to reset a simulator content and settings.
-      # If no `sim_udid` is nil, _all_ simulators are reset.
-      #
-      # # @note This is an Xcode 6 only method. It will raise an error if called on
-      #  Xcode < 6.
-      #
-      # @note This method will quit the simulator.
-      #
-      # @param [String] sim_udid The udid of the simulator that will be reset.
-      #   If sim_udid is nil, _all_ simulators will be reset.
-      # @raise [RuntimeError] If called on Xcode < 6.
-      # @raise [RuntimeError] If `sim_udid` is not a valid simulator udid.  Valid
-      #  simulator udids are determined by calling `simctl list`.
-      def simctl_reset(sim_udid = nil)
-        unless xcode_version_gte_6?
-          fail 'this method is only available on Xcode >= 6'
+puts "Monkeypatch: #{__FILE__}"
+RunLoop::SimControl.class_eval do
+  # @!visibility private
+  # Uses the `simctl erase` command to reset a simulator content and settings.
+  # If no `sim_udid` is nil, _all_ simulators are reset.
+  #
+  # # @note This is an Xcode 6 only method. It will raise an error if called on
+  #  Xcode < 6.
+  #
+  # @note This method will quit the simulator.
+  #
+  # @param [String] sim_udid The udid of the simulator that will be reset.
+  #   If sim_udid is nil, _all_ simulators will be reset.
+  # @raise [RuntimeError] If called on Xcode < 6.
+  # @raise [RuntimeError] If `sim_udid` is not a valid simulator udid.  Valid
+  #  simulator udids are determined by calling `simctl list`.
+  def simctl_reset(sim_udid = nil)
+    unless xcode_version_gte_6?
+      fail 'this method is only available on Xcode >= 6'
+    end
+
+    quit_sim
+
+    sim_details = sim_details(:udid)
+    simctl_erase = lambda { |udid|
+      # ################################################
+      puts '** MONKEYPATCH Speculative shutdown - may fail harmlessly'
+      shutdown_output = %x( xcrun simctl shutdown #{udid} )
+      puts "** MONKEYPATCH Injected simctl_shutdown: #{shutdown_output} [#{__FILE__}]"
+      # ################################################
+      args = "simctl erase #{udid}".split(' ')
+      Open3.popen3('xcrun', *args) do |_, stdout, stderr, wait_thr|
+        out = stdout.read.strip
+        err = stderr.read.strip
+        if ENV['DEBUG_UNIX_CALLS'] == '1'
+          cmd = "xcrun simctl erase #{udid}"
+          puts __FILE__ + " sim_erase cmd #{cmd}"
+          puts "#{cmd} => stdout: '#{out}' | stderr: '#{err}'"
         end
+        wait_thr.value.success?
+      end
+    }
 
-        quit_sim
-
-        sim_details = sim_details(:udid)
-        simctl_erase = lambda { |udid|
-          #################################################
-          shutdown_output = %x( xcrun simctl shutdown #{udid} )
-          puts __FILE__ + ' TB-BADOO Injected simctl_shutdown: ' + shutdown_output
-          #################################################
-          args = "simctl erase #{udid}".split(' ')
-          Open3.popen3('xcrun', *args) do |_, stdout, stderr, wait_thr|
-            out = stdout.read.strip
-            err = stderr.read.strip
-            if ENV['DEBUG_UNIX_CALLS'] == '1'
-              cmd = "xcrun simctl erase #{udid}"
-              puts __FILE__ + " sim_erase cmd #{cmd}"
-              puts "#{cmd} => stdout: '#{out}' | stderr: '#{err}'"
-            end
-            wait_thr.value.success?
-          end
-        }
-
-        # Call erase on all simulators
-        if sim_udid.nil?
-          res = []
-          sim_details.each_key do |key|
-            res << simctl_erase.call(key)
-          end
-          res.all?
-        else
-          if sim_details[sim_udid]
-            simctl_erase.call(sim_udid)
-          else
-            fail "Could not find simulator with udid '#{sim_udid}'"
-          end
-        end
+    # Call erase on all simulators
+    if sim_udid.nil?
+      res = []
+      sim_details.each_key do |key|
+        res << simctl_erase.call(key)
+      end
+      res.all?
+    else
+      if sim_details[sim_udid]
+        simctl_erase.call(sim_udid)
+      else
+        fail "Could not find simulator with udid '#{sim_udid}'"
       end
     end
   end
 end
-
-# RunLoop::SimControl.include RunLoop::SimControl::EraseFix
