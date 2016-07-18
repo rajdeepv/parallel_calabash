cc91b77a2b60fa50689025bf165aa3b81755bdc1
Rooth, Matthew <Matthew.Rooth@capitalone.com>
2016-07-18 13:56:10 +0100

    fixed device name matching

--- lib/parallel_calabash/feature_grouper.rb
+++ lib/parallel_calabash/feature_grouper.rb
@@ -69,7 +69,7 @@ module ParallelCalabash
         groups = Array.new(group_size) { [] }
         device_info.each_with_index do |device, device_index|
           matching_tags = device_tag_filters.map do |tag, device_ids|
-            tag unless device_ids.select { |curr_id| (device.first || '').match(curr_id) || (device.last || '').match(curr_id) }.empty?
+            tag unless device_ids.select { |curr_id| (device[0] || '').match(curr_id) || (device[1] || '').match(curr_id) }.empty?
           end.compact
           ensure_for_device_index_if_necessary(device_index, distribution_data, matching_tags)
         end
